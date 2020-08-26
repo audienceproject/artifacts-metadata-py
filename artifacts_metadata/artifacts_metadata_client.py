4cb4a0ba231845f15e2d5205cc485a2d27694b90
Cosmin Catalin Sanda <cosmincatalin@gmail.com>
2020-08-26 16:19:09 +0200

    Add automtic uuid

--- artifacts_metadata/artifacts_metadata_client.py
+++ artifacts_metadata/artifacts_metadata_client.py
@@ -22,7 +22,10 @@ class ArtifactsMetadataClient:
             dynamodb_resource = session.resource("dynamodb")
         self.table = dynamodb_resource.Table(table_name)
 
-    def log(self, artifact_type: str, metadata: Optional[Dict[str, object]] = None) -> str:
+    def getUuid(self) -> str:
+        return str(uuid.uuid1())
+
+    def log(self, artifact_type: str, uuid: str = getUuid, metadata: Optional[Dict[str, object]] = None) -> str:
         """
         Stores a dictionary of metadata into DynamoDB. Will always create a record,
         it does not update.
@@ -30,6 +33,7 @@ class ArtifactsMetadataClient:
         template variable that will be replaced by the id of the record to be inserted.
 
         @param artifact_type: A string categorization for artifacts.
+        @param uuid: Optionally hardcode the ID of the metadata artifact
         @param metadata: A dictionary of values. Can be deep, but must conform to the
             constraints of a DynamoDB PUT operation.
 
@@ -37,9 +41,10 @@ class ArtifactsMetadataClient:
         """
         if metadata is None:
             metadata = {}
-        metadata["id"] = str(uuid.uuid1())
+        metadata["id"] = uuid
         metadata["artifactType"] = artifact_type
-        metadata["timestamp"] = int(round(time.time() * 1000))
+        if "timestamp" not in metadata:
+            metadata["timestamp"] = int(round(time.time() * 1000))
         if "location" in metadata:
             metadata["location"] = metadata["location"].replace("{artifact-id}", metadata["id"])
         self.table.put_item(
@@ -56,3 +61,22 @@ class ArtifactsMetadataClient:
             a particular artifact id.
         """
         return self.table.get_item(Key={"id": artifact_id})["Item"]
+
+    def log_with_reference_time(self, artifact_type: str, yyyy: int, mm: int, dd: int, metadata: Optional[Dict[str, object]] = None) -> str:
+        metadata["yyyy"] = yyyy
+        metadata["mm"] = mm
+        metadata["dd"] = dd
+        return self.log(artifact_type, metadata)
+
+    def log_with_reference_time_as_date_string(self, artifact_type: str, date: str, metadata: Optional[Dict[str, object]] = None) -> str:
+        metadata["yyyy"] = int(date.split("-")[0])
+        metadata["mm"] = int(date.split("-")[1])
+        metadata["dd"] = int(date.split("-")[2])
+        return self.log(artifact_type, metadata)
+
+
+if __name__ == '__main__':
+    session = boto3.session.Session(region_name="us-east-1")
+    dynamodb_resource = session.resource("dynamodb")
+    artifacts_metadata_client = ArtifactsMetadataClient(dynamodb_resource=dynamodb_resource)
+    print(artifacts_metadata_client.get_latest_before("inference-labeled-data", 2020, 5, 1))
